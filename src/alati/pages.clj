a0f5377d206cfd45fe47b811eb8721655f94fdaf
NevenaDarijevic <darijevicnevena00@gmail.com>
2021-12-14 12:45:33 +0100

    Created: basePageTemplate which will be used for every page as base

--- src/alati/pages.clj
+++ src/alati/pages.clj
@@ -1,10 +1,14 @@
 (ns alati.pages
   (:require [hiccup.page :refer [html5]]                    ;https://github.com/weavejester/hiccup
             ))
+
+;Basic template for all pages, this is good practice
+(defn basePageTemplate [& body]
+  (html5
+    [:head [:title "Project-Articles"]]
+    [:body [:h1 "Articles"] body]))
+
 ;Creating of route for index page which displays all articles using hiccup pages
 (defn index [articles]
-  (html5 [:h1 "ARTICLES"]
-         (for [a articles]
-           [:h2 (:title a)])
-         )
- )
+  (basePageTemplate (for [a articles]
+                      [:h2 (:title a)])))
