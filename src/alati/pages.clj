6124cb29932b590df0f098ae94741a48138a5a70
NevenaDarijevic <darijevicnevena00@gmail.com>
2021-12-14 12:32:26 +0100

    Using hiccup pages for the first time in alati.pages for index page in index function

--- src/alati/pages.clj
+++ src/alati/pages.clj
@@ -1,6 +1,10 @@
-(ns alati.pages)
-;Creating of route for index page which displays all articles
+(ns alati.pages
+  (:require [hiccup.page :refer [html5]]                    ;https://github.com/weavejester/hiccup
+            ))
+;Creating of route for index page which displays all articles using hiccup pages
 (defn index [articles]
-  (->> articles
-       (map #(str "<h2>" (:title %) "</h2>"))
-       (apply str  "<h1>Testni naslov </h1>")))
+  (html5 [:h1 "ARTICLES"]
+         (for [a articles]
+           [:h2 (:title a)])
+         )
+ )
