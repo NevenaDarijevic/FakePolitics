af3275bb830603b89d1ecf7c8816f64dba167851
NevenaDarijevic <darijevicnevena00@gmail.com>
2021-12-30 18:07:05 +0100

    Added comments to db.clj, group functions

--- src/alati/db.clj
+++ src/alati/db.clj
@@ -13,6 +13,7 @@
 (def portalsCollection  "portals")
 (def commentsCollection  "comments")
 (def reportedNewsCollection  "reported")
+
 ;Extracting connection uri
 (def  dbConnectionUri  (or (System/getenv "alatiMongoUri")
                          "mongodb://127.0.0.1/alati-test"))
@@ -21,17 +22,7 @@
 (def db (-> dbConnectionUri
             mg/connect-via-uri
             :db))
-;REPL Insert new values into articles
-;(mc/insert alati.db/db "articles" {:title "First article" :body "Hello" :created (new java.util.Date)})
-;=> #object[com.mongodb.WriteResult 0x1ab4fb64 "WriteResult{n=0, updateOfExisting=false, upsertedId=null}"]
-;(mc/find-maps alati.db/db "articles")
-;=>
-;({:_id #object[org.bson.types.ObjectId 0xb2e784 "61b8743224950a19a40d3447"],
-;  :title "First article",
-;  :body "Hello",
-;  :created #inst"2021-12-14T10:38:42.738-00:00"})
-
-;Function which creates new article
+;Article functions
 (defn createArticle [title body author portal tag]
   (mc/insert db articlesCollection
              {:title   title
@@ -41,16 +32,6 @@
               :portal  portal
               :tag     tag}))
 
-(defn createComment [user article text ]
-  (mc/insert db commentsCollection
-             {:user   user
-              :article    article
-              :text text}))
-
-;Testing in REPL
-;(alati.db/createArticle "TestArticle" "...")
-
-;Function which updates article
 (defn updateArticle [art-id title body author portal tag]
   (mc/update-by-id db articlesCollection (ObjectId. art-id)
                    {$set{:title   title
@@ -60,66 +41,53 @@
                          :tag tag
                          }}))
 
-;Function which returns all articles
 (defn returnAllArticles []
-  (mc/find-maps db articlesCollection) )
-;Testing in REPL
-;(alati.db/returnAllArticles)
+  (mc/find-maps db articlesCollection))
 
-;Function which returns article by ID
 (defn returnArticleById [article-id]
   (mc/find-map-by-id db articlesCollection (ObjectId. article-id))) ;ObjectID is for importing ID
 
-;Delete article
+
 (defn deleteArticle [article-id]
   (mc/remove-by-id db articlesCollection (ObjectId. article-id)))
 
-(defn findPortalById [portal-id]
-  (mc/find-map-by-id db portalsCollection (ObjectId. portal-id)))
-
-(defn returnAllPortals []
-  (mc/find-maps db portalsCollection)  )
+;Comments functions
+(defn createComment [user article text ]
+  (mc/insert db commentsCollection
+             {:user   user
+              :article    article
+              :text text}))
 
 (defn findCommentsByArticleId [article-id]
-  (mc/find-maps db commentsCollection { :article article-id }) ;return lazy seq of maps
-  )
+  (mc/find-maps db commentsCollection { :article article-id })) ;return lazy seq of maps
 
+;Portals functions
+(defn returnAllPortals []
+  (mc/find-maps db portalsCollection))
+
+
+;Functions for filtering articles
 (defn findTrueNews []
-  (mc/find-maps db articlesCollection {:tag "true"} )) ;return lazy seq of maps)
+  (mc/find-maps db articlesCollection {:tag "true"} )) ;return lazy seq of maps
 
 (defn findFakeNews []
-  (mc/find-maps db articlesCollection  {:tag "false"} )) ;return lazy seq of maps)
+  (mc/find-maps db articlesCollection  {:tag "false"} )) ;return lazy seq of maps
 
 (defn findArticlesFromPortal [portal-name]
-  (mc/find-maps db articlesCollection {:portal portal-name })
-  )
+  (mc/find-maps db articlesCollection {:portal portal-name }))
+
+;Fuctions for statistics
 (defn countArticles []
-  (count(mc/find-maps db articlesCollection))
-  )
+  (count(mc/find-maps db articlesCollection)))
+
 (defn countArticlesFromPortal [portal-name]
-  (count(mc/find-maps db articlesCollection {:portal portal-name }))
-  )
+  (count(mc/find-maps db articlesCollection {:portal portal-name })))
+
 (defn countTrueArticles []
   (count(mc/find-maps db articlesCollection {:tag "true"} )))
+
 (defn countFakeArticles []
   (count(mc/find-maps db articlesCollection {:tag "false"} )))
-
-(defn reportFakeNew [link reason author portal]
-  (mc/insert db reportedNewsCollection
-             {:link   link
-              :reason    reason
-              :author  author
-              :portal  portal})
-  )
-
-(defn returnReported []
-  (mc/find-maps db reportedNewsCollection)  )
-
-(defn countReports []
-  (count(mc/find-maps db reportedNewsCollection)))
-
-(defn deleteReported [rep-id]
-  (mc/remove-by-id db reportedNewsCollection (ObjectId. rep-id)))
 
 (defn countTrueArticlesForPortal [portal]
   (count(mc/find-maps db articlesCollection {:tag "true" :portal portal} )))
@@ -130,10 +98,28 @@
 (defn findMaxFake []
   (let [maxFake (max (alati.db/countFalseArticlesForPortal "Blic") (alati.db/countFalseArticlesForPortal "Rts") (alati.db/countFalseArticlesForPortal "Politika"))]
     (if (= maxFake (alati.db/countFalseArticlesForPortal "Blic")) "Blic" (if (= maxFake (alati.db/countFalseArticlesForPortal "Rts")) "Rts" (if (= maxFake (alati.db/countFalseArticlesForPortal "Politika")) "Politika" ) ) )
-   ))
+    ))
 (defn findMaxTrue []
   (let [maxTrue (max (alati.db/countTrueArticlesForPortal "Blic") (alati.db/countTrueArticlesForPortal "Rts") (alati.db/countTrueArticlesForPortal "Politika"))]
     (if (= maxTrue (alati.db/countTrueArticlesForPortal "Blic")) "Blic" (if (= maxTrue (alati.db/countTrueArticlesForPortal "Rts")) "Rts" (if (= maxTrue (alati.db/countTrueArticlesForPortal "Politika")) "Politika" ) ) )
     ))
+
+
+;Functions for reporting fake news and reviewing reported news
+(defn reportFakeNew [link reason author portal]
+  (mc/insert db reportedNewsCollection
+             {:link   link
+              :reason  reason
+              :author  author
+              :portal  portal}))
+
+(defn returnReported []
+  (mc/find-maps db reportedNewsCollection)  )
+
+(defn countReports []
+  (count(mc/find-maps db reportedNewsCollection)))
+
+(defn deleteReported [rep-id]
+  (mc/remove-by-id db reportedNewsCollection (ObjectId. rep-id)))
 
 
